69bab7c76c7b62724a780224834cc5913ae27517
Rohan Singh <rohans212004@gmail.com>
2025-02-13 00:53:26 +0530

    Atlas_connnection

---
+++ backend/Models/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const mongo_url = process.env.MONGO_CONN;
+
+
+mongoose.connect(mongo_url)
+    .then(()=>{
+        console.log("Database Tripally Connected")
+    }).catch((err)=>{
+        console.log('MongoDB connection Error: ',err);
+    })
