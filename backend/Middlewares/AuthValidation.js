c14a848e1f25ea3af3326ca8f967809f26428972
Rohan Singh <rohans212004@gmail.com>
2025-02-14 23:52:18 +0530

    loginValidation

--- backend/Middlewares/AuthValidation.js
+++ backend/Middlewares/AuthValidation.js
@@ -6,6 +6,32 @@ const signupValidation = (req,res,next)=>{
         email: joi.string().email().required(),
         password: joi.string().min(4).max(100).required()
     })
+
+    const {error} = schema.validate(req.body);
+    if(error){
+        return res.status(400)
+        .json({message: "Bad request", error})
+    }
+    next();
 }
 
-const {error} = schema.validate(req.body);
+
+const loginValidation = (req,res,next)=>{
+    const schema = joi.object({
+        email: joi.string().email().required(),
+        password: joi.string().min(4).max(100).required()
+    })
+
+    const {error} = schema.validate(req.body);
+    if(error){
+        return res.status(400)
+        .json({message: "Bad request", error})
+    }
+    next();
+}
+
+
+module.exports = {
+    signupValidation,
+    loginValidation
+}
